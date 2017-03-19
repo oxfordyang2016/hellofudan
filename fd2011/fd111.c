809b40b222e7b44ac778318feaa772e8e50cdbf4
root <root@>
2017-03-20 02:15:38 +0800

    get string

---
+++ fd2011/fd111.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1[100],s2[100],temp[100],temp2[100];
+int i,j;
+scanf("%s%s",s1,s2);
+if(strlen(s2)>strlen(s1))
+{
+strcpy(temp,s1);
+strcpy(s1,s2);
+strcpy(s2,temp);
+}
+temp[0]=0;//方便后续操作
+for(i=0;i<strlen(s2);i++)
+for(j=strlen(s2);j>i;j--)
+{
+memset(temp2,0,sizeof(temp2));//增加了这一句
+strncpy(temp2,s2+i,j-i);
+if(strstr(s1,temp2))
+{
+if(strlen(temp2)>strlen(temp))
+strcpy(temp,temp2);
+}
+}
+printf("%s\n",temp);
+return 0;
+}
